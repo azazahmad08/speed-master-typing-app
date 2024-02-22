ad2b4c3802d7f99269866dfbce77742940220cdc
azazahmad08 <engr.azaz08@gmail.com>
2024-02-22 18:11:25 +0600

    Done

--- scripts/pro_typer.js
+++ scripts/pro_typer.js
@@ -4,9 +4,52 @@
 //     const playGround = document.getElementById('play-ground');
 //     playGround.classList.remove('hidden')
 // }
+function handleKeyboardButtonPress(event) {
+    // variable to get key
+    const playerPressed = event.key;
+
+    // current alphabet khoja
+    const currentAlphabets = document.getElementById('display_Alphabets');
+    const currentAlpha = currentAlphabets.innerText;
+
+    // lowercase
+    const alphabet = currentAlpha.toLowerCase();
+    console.log(playerPressed, alphabet)
+
+
+    if (playerPressed === alphabet) {
+        const scoreGet = document.getElementById('score');
+        const score = scoreGet.innerText;
+        const scoreNumber = parseInt(score);
+        // console.log(scoreNumber);
+        const scoreNumberUpdate = scoreNumber + 1;
+        scoreGet.innerText = scoreNumberUpdate;
+
+        removeBackgroundColor(alphabet);
+        continueGame();
+    }
+    else {
+        const lifeGet = document.getElementById('life');
+        const life = lifeGet.innerText;
+        const lifeUpdated = parseInt(life);
+        // console.log(scoreNumber);
+        const lifeWeGetUpdated = lifeUpdated - 1;
+        lifeGet.innerText = lifeWeGetUpdated;
+
+        if(lifeGet === 0){
+            hideElementById('play-ground');
+            showElementById('result');
+            
+        }
+
+    }
+}
+
+document.addEventListener('keyup', handleKeyboardButtonPress)
+
 function continueGame() {
     const alphabet = getRandomAlphabets();
-    console.log(alphabet);
+    // console.log(alphabet);
     const displayAlphabet = document.getElementById('display_Alphabets');
     displayAlphabet.innerText = alphabet;
     setBackgroundColor(alphabet);
--- scripts/utility.js
+++ scripts/utility.js
@@ -10,6 +10,10 @@ function setBackgroundColor(elementId){
     const element = document.getElementById(elementId);
     element.classList.add("bg-orange-400");
 }
+function removeBackgroundColor(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove("bg-orange-400");
+}
 
 function getRandomAlphabets(){
     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
